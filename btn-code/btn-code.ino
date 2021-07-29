56d8ec10120351e17f4ae77173cbb20c5b394839
Nouman Mahmood <noumanmhd@outlook.com>
2021-07-29 23:57:40 +0500

    Add Button Code

---
+++ btn-code/btn-code.ino
@@ -0,0 +1,67 @@
+///////////////////////////////
+// Please Change the PIN (PA7)
+#define BTN_PIN PA7 
+#define GAP_DELAY 1000
+#define TAP_MAX_DELAY 400
+//////////////////////////////
+
+#define SAMPLES_SIZE 20
+#define SAMPLES_DELAY 1
+
+
+long count = 0;
+unsigned long t = 0;
+unsigned long dt = 0;
+
+unsigned int tap = 0;
+unsigned int hold = 0;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(BTN_PIN, INPUT_PULLDOWN);
+}
+
+void loop() {
+  dt = millis() - t;
+  if(readInput(BTN_PIN)){
+    unsigned int press_time = pressRoutine();
+    if(press_time < TAP_MAX_DELAY){
+      tap++;
+    }
+    else{
+      hold++;
+    }
+    t = millis();
+    dt = millis() - t;
+  }
+ 
+  if(dt > GAP_DELAY){
+    if (tap > 0 || hold > 0){
+      Serial.print("TAP ");
+      Serial.print(tap);
+      Serial.print(" - ");
+      Serial.print("HOLD ");
+      Serial.println(hold);
+      tap = 0;
+      hold = 0;
+    }
+  }
+}
+
+bool readInput(byte pin){
+  double sum = 0;
+  for (int i=0; i<SAMPLES_SIZE; i++){
+    sum += digitalRead(pin);
+    delay(SAMPLES_DELAY);
+  }
+  sum = round(sum/SAMPLES_SIZE);
+  return (sum == 1);
+}
+
+unsigned long pressRoutine(){
+  unsigned long hold_t = millis();
+  while(readInput(BTN_PIN));
+  hold_t = millis() - hold_t;
+  return hold_t;
+}
